5ae7722d75c167b7c7cd384fef32e07e4a8297e1
Alanolog <alanrutyna2003@gmail.com>
2023-01-05 12:26:23 +0100

    add tailwind instead of styled components

---
+++ components/Announcement.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Announcement = () => {
+  return (
+    <div className=" h-8 bg-teal-500 text-white flex items-center justify-center text-sm">
+      Super Deal! Second product -50% OFF
+    </div>
+  );
+};
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,82 +1,30 @@
 import React from "react";
-import styled from "styled-components";
 import { BiSearch } from "react-icons/bi";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
-const Container = styled.div`
-  height: 60px;
-`;
-
-const Wrapper = styled.div`
-  padding: 10px 20px;
-  display: flex;
-  justify-content: space-between;
-`;
-const Left = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-`;
-const Language = styled.span`
-  font-size: 14px;
-  cursor: pointer;
-`;
-const SearchContainer = styled.div`
-  border: 1px solid lightgray;
-  display: flex;
-  align-items: center;
-  margin-left: 25px;
-  padding: 5px;
-  gap: 4px;
-`;
-const Input = styled.input`
-  border: none;
-`;
-
-const Center = styled.div`
-  flex: 1;
-  text-align: center;
-`;
-
-const Logo = styled.h1`
-  font-weight: 600;
-`;
-
-const Right = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-`;
-
-const MenuItem = styled.div`
-  flex: 1;
-  font-size: 14px;
-  cursor: pointer;
-  margin-left: 10px;
-`;
-
 export const Navbar = () => {
   return (
-    <Container>
-      <Wrapper>
-        <Left>
-          <Language>EN</Language>
-          <SearchContainer>
-            <Input />
+    <div className=" h-16">
+      <div className=" px-3 py-5 flex justify-between">
+        <div className=" flex-1 flex items-center border border-gray-400">
+          <span className=" text-sm cursor-pointer">EN</span>
+          <div className=" flex items-center ml-6 p-1 gap-1">
+            <input className=" border-none" />
             <BiSearch style={{ color: "gray", fontSize: 16 }} />
-          </SearchContainer>
-        </Left>
-        <Center>
-          <Logo>RANDOM</Logo>
-        </Center>
-        <Right>
-          <MenuItem>REGISTER</MenuItem>
-          <MenuItem>SIGN IN</MenuItem>
-          <MenuItem>
+          </div>
+        </div>
+        <div className=" flex-1 text-center ">
+          <h1 className=" font-semibold">RANDOM</h1>
+        </div>
+        <div className=" flex-1 flex items-center ">
+          <div className=" flex-1 text-sm cursor-pointer ml-3">REGISTER</div>
+          <div className=" flex-1 text-sm cursor-pointer ml-3">SIGN IN</div>
+          <div className=" flex-1 text-sm cursor-pointer ml-3">
+            {" "}
             <AiOutlineShoppingCart />
-          </MenuItem>
-        </Right>
-      </Wrapper>
-    </Container>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
